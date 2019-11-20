2e8da0843200017326727bfd3731bf89ab481136
DUY.TRAM <DUY.TRAM@s124-12.pedago.mermoz.edu>
2019-11-20 17:46:22 +0100

    update tower

--- src/towers/Terrain.java
+++ src/towers/Terrain.java
@@ -1,14 +1,16 @@
 package towers;
 
 public class Terrain {
+	
+	int mapx = 100;
+	int mapy= 100;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Ennemi grosMechant = new Ennemi();
-		grosMechant.type = "Gros méchant";
-		
-		grosMechant.setPv(200);
+		grosMechant.type = "Gros méchant";		
+		grosMechant.setPv(300);
 		grosMechant.setSpeed(4);
 
 		System.out.println(grosMechant);
@@ -17,13 +19,20 @@ public class Terrain {
 		System.out.println(leVraiGrosEnnemi);
 		
 		grosMechant.seDeplace(5, 8);
-		grosMechant.recevoirDegat(50);
 		System.out.println(grosMechant);
 		System.out.println("total ennemi "+Ennemi.compteur);
 		
-		Ennemi nouveauEnnemi = Ennemi.saisirEnnemi();
-		System.out.println(nouveauEnnemi);
+//		Ennemi nouveauEnnemi = Ennemi.saisirEnnemi();
+//		System.out.println(nouveauEnnemi);
 		
+		Tour tour1 = new Tour();
+		
+		tour1.attack(grosMechant);
+		System.out.println(grosMechant);
+		tour1.attack(grosMechant);
+		tour1.attack(grosMechant);
+		tour1.attack(grosMechant);
+		System.out.println(grosMechant);
 		
 	}
 
--- src/towers/Tour.java
+++ src/towers/Tour.java
@@ -2,4 +2,29 @@ package towers;
 
 public class Tour {
 
+	private int force;
+	private int porte;
+	private float posX;
+	private float posY;
+	
+	
+	public Tour(int force, int porte, int posX, int posY) {
+		this.force = force;
+		this.porte = porte;
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	
+	
+	public Tour() {
+		this.force = 10;
+		this.porte = 5;
+
+	}
+	
+	public void attack(Ennemi target) {
+		System.out.println(target.type+ " a été attaqué");
+		target.recevoirDegat(this.force);
+	}
 }
